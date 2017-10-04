a600ac4fd68f263b73fdb93d71533e32b38abb1a
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2017-10-04 16:22:41 -0600

    Add laravel to spanish and Controllers

---
+++ app/Http/Controllers/FabricanteAvionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+//Se necesitan los 2 modelos
+use App\Avion;
+use App\Fabricante;
+use Illuminate\Http\Request;
+
+class FabricanteAvionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //Ojo que recibe un id_fabricante
+    public function index($id_fabricante)
+    {
+        // Devolverá todos los aviones.
+        //return "Mostrando los aviones del fabricante con Id $idFabricante";
+        $fabricante = Fabricante::find($id_fabricante);
+
+        if(!fabricante){
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>'404','message'=>'No se encontro ningun fabricante con ese codigo']),404]);
+        }
+        return response()->json(['status'=>'ok', 'data'=>$fabricante->aviones()->get()],200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($id_fabricante)
+    {
+        //
+        return "Se muestra formulario para crear un avion del fabricante $id_fabricante";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id_fabricante, $id_avion)
+    {
+        //
+        return "Se muestra avion $id_avion del fabricante $id_fabricante";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id_fabricante, $id_avion)
+    {
+        //
+        return "Se muestra formulario para editar el avion $id_avion del fabricante $id_fabricante";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// resource recibe nos parámetros(URI del recurso, Controlador que gestionará las peticiones)
+Route::resource('fabricante','FabricanteController', ['except'=>['edit','create']]); //Todos los metodos menos Edit  que mostraria un formulario
+
+
+// Si queremos dar  la funcionalidad de ver todos los aviones tendremos que crear una ruta específica.
+// Pero de aviones solamente necesitamos solamente los métodos index y show.
+// Lo correcto sería hacerlo así:
+Route::resource('aviones','AvionController',[ 'only'=>['index','show'] ]); //El resto se gestionan en FabricanteAvionController
+
+
+// Como la clase principal es fabricantes y un avión no se puede crear si no le indicamos el fabricante,
+// entonces necesitaremos crear lo que se conoce como  "Recurso Anidado" de fabricantes con aviones.
+// Definición del recurso anidado:
+Route::resource('fabricante.aviones','FabricanteAvionController');
