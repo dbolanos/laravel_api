578c519f965392462a63854c1f85e187a21bcd67
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2017-10-03 23:09:32 -0600

    add seeders for Fabricante and Avion

--- database/seeds/AvionSeeder.php
+++ database/seeds/AvionSeeder.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
+//HAce uso del modelo de Fabricante
+use App\Fabricante;
+
+//Hace uso del modelo Avion
+use App\Avion;
+
+//Le indicamos  que utilice tambien Faker
+// Información sobre Faker: https://github.com/fzaninotto/Faker
+use Faker\Factory as Faker;
 class AvionSeeder extends Seeder
 {
     /**
@@ -11,6 +20,31 @@ class AvionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Creamos una instancia de Faker
+        $faker = Faker::create();
+
+        //Para cubrir los aviones tenemos que tener en cuentaque fabricantes tenemos.
+        //Averiguamos cuantos fabricantes hay en la tabla
+
+        $cuantos = Fabricante::all()->count();
+
+        //Creamos un bucle para crear 20 aviones:
+
+        for($i=0;$i<19;$i++){
+            //Cuando llamamos al metodo create del modelo Avion
+            //se esta creando una nueva fila en la tabla
+            Avion::create(
+                [
+                    'modelo'=>$faker->word(),
+                    'longitud'=>$faker->randomFloat(2,10,150),
+                    'capacidad'=>$faker->randomNumber(3), //de 3 digitos como maximo
+                    'velocidad'=>$faker->randomNumber(4), //de 4 digitos como maximo
+                    'alcance'=>$faker->randomNumber(),
+                    'fabricante_id'=>$faker->numberBetween(1,$cuantos)
+                ]
+            );
+        }
+
+
     }
 }
--- database/seeds/FabricanteSeeder.php
+++ database/seeds/FabricanteSeeder.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Database\Seeder;
 
+// Hace uso del modelo de Fabricante.
+use App\Fabricante;
+
+// Le indicamos que utilice también Faker.
+// Información sobre Faker: https://github.com/fzaninotto/Faker
+use Faker\Factory as Faker;
+
+
 class FabricanteSeeder extends Seeder
 {
     /**
@@ -11,6 +19,20 @@ class FabricanteSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Creamos una instancia de Faker
+        $faker = Faker::create();
+
+        //Creamos un bucle para cubrir 5 fabricanttes:
+        for($i=0; $i<4; $i++){
+            //Cuando llamamos al metodo create del Modelo Fabricante
+            //Se esta creando una nueva fila en la tabla.
+            Fabricante::create(
+                [
+                    'nombre' =>$faker->word(),
+                    'direccion'=>$faker->word(),
+                    'telefono'=>$faker->randomNumber(9) //de 9 digitos como maximo
+                ]
+            );
+        }
     }
 }
